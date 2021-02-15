3ecb8020a133025ee0e47e62f8c04d0f7d756ef1
Muhammad Bilal <Bscs.f18.m16.a@gmail.com>
2021-02-15 16:40:17 +0500

    jhga

--- public/SW.js
+++ public/SW.js
@@ -23,36 +23,61 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log("Fetch runssssssssssssss")
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
+// self.addEventListener('fetch', function(event) {
+//     console.log("Fetch runssssssssssssss")
+//     event.respondWith(
+//       caches.match(event.request)
+//         .then(function(response) {
+//           // Cache hit - return response
+//           if (response) {
+//             return response;
+//           }
+//           return fetch(event.request).then(
+//             function(response) {
+//               // Check if we received a valid response
+//               if(!response || response.status !== 200 || response.type !== 'basic') {
+//                 return response;
+//               }
   
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              var responseToCache = response.clone();
+//               // IMPORTANT: Clone the response. A response is a stream
+//               // and because we want the browser to consume the response
+//               // as well as the cache consuming the response, we need
+//               // to clone it so we have two streams.
+//               var responseToCache = response.clone();
   
-              caches.open(CACHE_NAME)
+//               caches.open(CACHE_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request, responseToCache);
+//                 });
+  
+//               return response;
+//             }
+//           );
+//         })
+//       );
+//   });
+
+
+addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(CACHE_NAME)
                 .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-  
-              return response;
-            }
-          );
-        })
-      );
-  });
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(      // fallback mechanism
+              console.log("Errors")
+            )
+            
+        }
+      })
+  );
+});  
